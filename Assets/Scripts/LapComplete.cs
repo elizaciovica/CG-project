157eb4c3cfa56213dac601405cff2351487c697a
Eliza <elizaciovica0208@gmail.com>
2022-05-22 14:32:04 +0300

    lap working

--- Assets/Scripts/LapComplete.cs
+++ Assets/Scripts/LapComplete.cs
@@ -13,25 +13,42 @@ public class LapComplete : MonoBehaviour {
 	public GameObject SecondDisplay;
 	public GameObject MilliDisplay;
 
+	public GameObject LapCounter;
+	public int LapsDone;
+
+	//public float RawTime;
+
 	void OnTriggerEnter () {
+		LapsDone += 1;
+		//RawTime = PlayerPrefs.GetFloat("RawTime");
+		//if(LapTimeManager.RawTime <= RawTime) {
 
-		if (LapTimeManager.SecondCount <= 9) {
-			SecondDisplay.GetComponent<TextMeshProUGUI> ().text = "0" + LapTimeManager.SecondCount + ".";
-		} else {
-			SecondDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.SecondCount + ".";
-		}
+			if (LapTimeManager.SecondCount <= 9) {
+				SecondDisplay.GetComponent<TextMeshProUGUI> ().text = "0" + LapTimeManager.SecondCount + ".";
+			} else {
+				SecondDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.SecondCount + ".";
+			}
 
-		if (LapTimeManager.MinuteCount <= 9) {
-			MinuteDisplay.GetComponent<TextMeshProUGUI> ().text = "0" + LapTimeManager.MinuteCount + ":";
-		} else {
-			MinuteDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.MinuteCount + ":";
-		}
+			if (LapTimeManager.MinuteCount <= 9) {
+				MinuteDisplay.GetComponent<TextMeshProUGUI> ().text = "0" + LapTimeManager.MinuteCount + ":";
+			} else {
+				MinuteDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.MinuteCount + ":";
+			}
 
-		MilliDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.MilliCount.ToString("F0");
+			MilliDisplay.GetComponent<TextMeshProUGUI> ().text = "" + LapTimeManager.MilliCount.ToString("F0");
+		//}
+
+		PlayerPrefs.SetInt("MinSave", LapTimeManager.MinuteCount);
+		PlayerPrefs.SetInt("SecSave", LapTimeManager.SecondCount);
+		PlayerPrefs.SetFloat("MilliSave", LapTimeManager.MilliCount);
+		//PlayerPrefs.SetFloat("RawTime", LapTimeManager.RawTime);
 
 		LapTimeManager.MinuteCount = 0;
 		LapTimeManager.SecondCount = 0;
 		LapTimeManager.MilliCount = 0;
+		//LapTimeManager.RawTime = 0;
+
+		LapCounter.GetComponent<TextMeshProUGUI> ().text = "" + LapsDone;
 
 		HalfLapTrig.SetActive (true);
 		LapCompleteTrig.SetActive (false);
---
+++ Assets/Scripts/LoadLapTime.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadLapTime : MonoBehaviour {
+
+	public int MinCount;
+	public int SecCount;
+	public float MilliCount;
+	public GameObject MinDisplay;
+	public GameObject SecDisplay;
+	public GameObject MilliDisplay;
+
+	void Start () {
+
+		MinCount = PlayerPrefs.GetInt ("MinSave");
+		SecCount = PlayerPrefs.GetInt ("SecSave");
+		MilliCount = PlayerPrefs.GetFloat ("MilliSave");
+
+		MinDisplay.GetComponent<TextMeshProUGUI> ().text = "" + MinCount + ":";
+		SecDisplay.GetComponent<TextMeshProUGUI> ().text = "" + SecCount + ".";
+		MilliDisplay.GetComponent<TextMeshProUGUI> ().text = "" + MilliCount.ToString("F0");
+		
+	}
+	
+
+}
